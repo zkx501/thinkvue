fa81ac4485c7fdbaa696a4184d897f2dec9633ef
zkx <zkx@example.com>
2017-07-27 15:11:38 +0800

    初步测试thinkphp的功能模块

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,15 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+//    //开启子域名部署
+//    'APP_SUB_DOMAIN_DEPLOY' => true,
+//
+//    //子域名访问规则
+//    'APP_SUB_DOMAIN_RULES'  => array(
+//        'manage'
+//    ),
+    //默认访问的模块
+    'DEFAULT_MODULE' => 'Home',
+    //允许访问的模块
+    'MODULE_ALLOW_LIST'    =>    array('Home','Manage'),
 );
